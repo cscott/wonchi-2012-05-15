e5e8e0282fe0f5b9961656082391b80bd2a5c6f4
C. Scott Ananian <cscott@cscott.net>
2012-05-21 20:26:56 -0400

    Update the scatterplot code; generate scatterplots w/ all user actions.

--- src/scatter.js
+++ src/scatter.js
@@ -1,14 +1,30 @@
 #!/usr/bin/env node
 
 var requirejs = require('requirejs');
-requirejs(['commander', 'csv'], function(program, csv) {
+requirejs(['commander', 'csv', 'fs', 'printf', './version'], function(program, csv, fs, printf, version) {
 
     var SUPPRESS_PAUSES = false;
     var USE_INDEXES = false;
 
     program
-        //.version(version)
+        .version(version)
         .usage('[options] <csvfile> ... <csvfile>')
+        .option('-V, --version <balloons version>',
+                'Specify the version of nell-balloons which generated this csv',
+                1)
+        .option('-x, --xdata <indexed|nopause|realtime>',
+                'Specify what sort of data to emit on the x axis',
+                'indexed')
+        .option('-y, --ydata <correct|escape|incorrect|start|all>',
+                'Specify what sort of data to emit on the y axis',
+                'correct')
+        .option('-t, --template <output filename template>',
+                'Output template for data & gnuplot files',
+                '%(num)s-%(xdata)s-%(ydata)s')
+        .option('-g, --gnuplot',
+                'Output gnuplot script for data')
+        .option('-G, --gnuplot-all',
+                'Output gnuplot script combining all ydata options')
         .parse(process.argv);
 
     if (program.args.length===0) {
@@ -16,25 +32,57 @@ requirejs(['commander', 'csv'], function(program, csv) {
         return;
     }
 
-    program.args.forEach(function(csvfile) {
+    var ydatalist = (program.ydata==='all') ? ['correct', 'escape', 'incorrect', 'start'] : [ program.ydata ];
+    ydatalist.forEach(function(ydata) { program.args.forEach(function(csvfile, csvidx) {
+        // look for a good number to use
+        var csvm = csvfile.match(/\/csv_([0-9]+)\/[^\/]*\.csv$/);
+        var csvnum = csvm ? csvm[1] : 'unk';
+
+        // output filename
+        var templateData =  {
+            num: csvnum,
+            full: csvfile,
+            index: csvidx,
+            xdata: program.xdata,
+            ydata: ydata,
+            version: program.version
+        };
+        var outfilename = printf(program.template, templateData);
+        var dataOutput = fs.createWriteStream(outfilename+'.data',
+                                              { encoding: 'utf-8' });
+
         var COLORS = ['yellow', 'black', 'lilac', 'orange', 'unknown'];
         var alive = {};
         COLORS.forEach(function(color) { alive[color] = []; });
         var pauseTime = 0;
 
         var wasPaused = 0, outIndex = 0;
-        var outCorrect = [], outFirstTime=null;
+        var outFirstTime=null;
         var doLog = function(which, timestamp, color, time, balloon) {
+            var point = null;
             timestamp = +timestamp;
             if (outFirstTime===null) { outFirstTime = timestamp; }
-            else if (wasPaused && SUPPRESS_PAUSES) {
+            else if (wasPaused && program.xdata === 'nopause') {
                 outFirstTime += wasPaused;
                 wasPaused = 0;
             }
-            if (which==='correct') {
-                outCorrect.push([USE_INDEXES ? (outIndex++) :
-                                 (timestamp - outFirstTime),
-                                 time - (balloon.pauseTime||0)]);
+            var x = (program.xdata === 'indexed') ?
+                outIndex : (timestamp - outFirstTime);
+            if (which==='correct' && ydata === 'correct') {
+                point = [x, time - (balloon.pauseTime||0)];
+            }
+            if (which==='incorrect' && ydata === 'incorrect') {
+                point = [x, time];
+            }
+            if (which==='escape' && ydata === 'escape') {
+                point = [x, time - (balloon.pauseTime||0)];
+            }
+            if (which==='start' && ydata === 'start') {
+                point = [x, 20000];
+            }
+            outIndex++;
+            if (point) {
+                dataOutput.write(point[0]+'\t'+point[1]+'\n');
             }
         };
 
@@ -127,6 +175,7 @@ requirejs(['commander', 'csv'], function(program, csv) {
                     alive['unknown'].push({ born: data.timestamp });
                     alive['unknown'].push({ born: data.timestamp });
                     //console.error('startup', index);
+                    doLog('start', data.timestamp, null, 0, null);
                     break;
                 case 'colorchange':
                 case 'highscore':
@@ -139,12 +188,56 @@ requirejs(['commander', 'csv'], function(program, csv) {
                 }
             }).
             on('end', function(count) {
-                outCorrect.forEach(function(x) {
-                    console.log(x[0], x[1]);
+                var gp = [];
+                if (program.gnuplot) { gp.push(false); }
+                if (program.gnuplotAll) { gp.push(true); }
+                gp.forEach(function(isAll) {
+                    var gpOutput;
+                    var gpfilename = outfilename;
+                    if (isAll) {
+                        var t = Object.create(templateData);
+                        t.ydata = 'all';
+                        gpfilename = printf(program.template, templateData);
+                    };
+                    gpOutput = fs.createWriteStream(gpfilename+'.gnuplot',
+                                                    { encoding: 'utf-8' });
+                    gpOutput.write(
+                        'set terminal postscript landscape color\n'+
+                        'set output "'+gpfilename+'.ps"\n'+
+                        'set title "'+csvfile+'"\n');
+                    var xlabel;
+                    if (program.xdata === 'indexed') {
+                        xlabel = "# of interactions";
+                    } else if (program.xdata === 'nopause') {
+                        xlabel = "Application use time (ms)";
+                    } else if (program.xdata === 'realtime') {
+                        xlabel = "Real time (ms)";
+                    }
+                    if (xlabel) {
+                        gpOutput.write('set xlabel "'+xlabel+'"\n');
+                    }
+                    gpOutput.write('set ylabel "Time (ms)"\n');
+                    gpOutput.write('set yrange [0:30000]\n');
+                    if (!isAll) {
+                        gpOutput.write('plot "'+outfilename+'.data"\n');
+                    } else {
+                        var files = ['correct', 'incorrect', 'escape', 'start'].
+                            map(function(type) {
+                                var t = Object.create(templateData);
+                                t.ydata = type;
+                                var f = printf(program.template, t);
+                                return f;
+                            }).map(function(f) {
+                                return '"'+f+'.data"';
+                            });
+                        gpOutput.write('plot '+files[1]+', '+files[0]+', '+
+                                       files[2]+', '+files[3]+' with impulses'+
+                                       '\n');
+                    }
                 });
             }).
             on('error', function(error) {
                 console.error(error.message);
             });
-    });
+    }); });
 });
