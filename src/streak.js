214e52e59344cb2f119d640940fecfac9568d03e
C. Scott Ananian <cscott@cscott.net>
2012-05-21 21:35:11 -0400

    Update streak-counting code; count incorrect streaks, ignore escaped.

--- src/streak.js
+++ src/streak.js
@@ -16,26 +16,45 @@ requirejs(['commander', 'csv'], function(program, csv) {
     program.args.forEach(function(csvfile) {
         var numConsecutiveCorrect = 0;
         var maxConsecutiveCorrect = 0;
+
+        var numConsecutiveIncorrect = 0;
+        var maxConsecutiveIncorrect = 0;
         csv().
             fromPath(csvfile, { columns: true }).
             on('data', function(data, index) {
+                var m;
                 // data has fields 'id', 'device', 'timestamp', 'name', 'value'
                 switch (data.name) {
                 case 'incorrect':
+                    m = data.value.match(/^escape.([a-z]+):([0-9]+)/);
+                    if (m) {
+                        break; /* ignore */
+                    }
+                    m = data.value.match(/^click.([a-z]+):([0-9]+)/);
+                    console.assert(m);
+
+                    numConsecutiveIncorrect++;
+                    maxConsecutiveIncorrect = Math.max(maxConsecutiveIncorrect,
+                                                       numConsecutiveIncorrect);
+
                     numConsecutiveCorrect = 0;
                     break;
                 case 'correct':
                     numConsecutiveCorrect++;
                     maxConsecutiveCorrect = Math.max(maxConsecutiveCorrect,
                                                      numConsecutiveCorrect);
+
+                    numConsecutiveIncorrect = 0;
                     break;
                 }
                 //console.log('#'+index+' '+JSON.stringify(data));
             }).
             on('end', function(count) {
                 console.log("Stats for: "+csvfile);
-                console.log("- Largest consecutive correct streak: " +
+                console.log("- Largest consecutive correct streak:   " +
                             maxConsecutiveCorrect);
+                console.log("- Largest consecutive incorrect streak: " +
+                            maxConsecutiveIncorrect);
             }).
             on('error', function(error) {
                 console.error(error.message);
